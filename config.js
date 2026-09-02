3eecfdbb2fe14303ea9f35553f2cb4ff07eb1bca
vnrchoicez <vnrchoisez@gmail.com>
2026-09-02 21:06:43 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const SUPABASE_URL = "https://fydaouflujmrvpmjnbmb.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY =
   "sb_publishable_IlEnkKMEdYNkzR2E8v18UQ_uxtIBnwQ";
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_PUBLISHABLE_KEY
 );
